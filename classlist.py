ed0789c1fa2ac22adfe4917b2f41b8b784208dc0
leng3 <leng3@stuy.edu>
2017-09-13 20:19:24 -0400

    adding classlist.py

---
+++ classlist.py
@@ -0,0 +1,22 @@
+'''
+Connie Lei & Lisa Eng
+SoftDev pd8
+Work #02--Get Your Pyth-On
+2017-09-12
+'''
+
+from random import randint
+
+CLASSES = {
+    7: [ 'Helen', 'Shakil', 'Eric', 'Jennifer Y', 'Jennifer Z', 'Arif', 'Queenie', 'Jawadul', 'Shaina', 'Vivien', 'Brian', 'Naotaka', 'Bayan', 'Adam', 'Caleb', 'Terry', 'Jason', 'Alessandro', 'Samantha', 'Carol', 'Joyce', 'Shannon', 'Charles', 'Taylor', 'Kelly', 'Leo', 'Khyber', 'Ibnul', 'Eugene', 'Yuyang', 'Karina', 'Tiffany', 'Holden', 'Michael'],
+    8: ['Masha', 'Adrian', 'David', 'Eric', 'Josh', 'Jerome', 'Henry', 'Jackie', 'Giorgio', 'Karen', 'Sonal', 'Xavier', 'Bermet', 'Alex', 'Iris', 'Manahal', 'Donia', 'Md', 'Connie', 'Lisa', 'Xing', 'Angelica', 'Angel', 'Augie', 'Dimitriy', 'Yiduo', 'Gordon', 'Tiffany', 'Clive', 'Jonathan', 'Sasha', 'Daniel'],
+    9: [ 'Yu Qi', 'Michela', 'Kristin', 'Fabiha', 'Maxim', 'Marcus', 'Ish', 'James', 'Ryan', 'Edward', 'Adeeb', 'Jake', 'Cynthia', 'Kevin', 'Levi', 'Edmond', 'Kyle', 'Andrew', 'Max', 'Jenny', 'Philip', 'Shan', 'Mansour', 'Ray', 'Jake', 'Ida', 'Kerry', 'Stanley', 'Jackie', 'William', 'Tina', 'Michael']
+}
+
+def name():
+    period = int(raw_input("Select class period: "))
+    while(period != 8 and period != 7 and period != 9):
+        period = int(raw_input("Choose only 7, 8, 9\n"))
+    print CLASSES[period][randint(0, len(CLASSES[period]))]
+    
+name()
